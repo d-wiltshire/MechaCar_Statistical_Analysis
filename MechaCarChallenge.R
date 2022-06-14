da3be7c80d80ef898e3bdf67fe0b3a31c96f1dcc
d-wiltshire <100863488+d-wiltshire@users.noreply.github.com>
2022-06-14 09:33:40 -0400

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -6,13 +6,11 @@ mechacar_df <- read.csv(file='MechaCar_mpg.csv')
 
 head(mechacar_df)
 
-#lm(qsec ~ mpg + disp + drat + wt + hp,data=mtcars) #generate multiple linear regression model
-
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechacar_df) #generate multiple linear regression model
 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechacar_df))
 
-## "performed on all six variables"?
+
 
 
 
@@ -21,8 +19,6 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 s_coil_table <- read.csv(file='Suspension_Coil.csv')
 
 head(s_coil_table)
-
-?summarize()
 
 total_summary <- s_coil_table %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
 
